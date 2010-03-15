45d5876f97a23a647b6782c57d9a70a392e1de2c
M66B <M66B@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-15 21:30:53 +0000

    2010
    git-svn-id: https://plugins.svn.wordpress.org/one-time-password/tags/2.8.3@217980 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-otp.php
+++ wp-otp.php
@@ -9,7 +9,7 @@ Author URI: http://blog.bokhorst.biz/about/
 */
 
 /*
-	Copyright 2009  Marcel Bokhorst
+	Copyright 2009, 2010 Marcel Bokhorst
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
